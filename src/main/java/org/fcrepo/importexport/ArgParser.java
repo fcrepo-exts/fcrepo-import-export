6687cf8f0dc142e263f3c28eb62fd7a51f35dbd7
Andrew Woods <awoods@duraspace.org>
2016-08-29 19:19:26 -0400

    Add CommandLineParsing framework
    Resolves: https://jira.duraspace.org/browse/FCREPO-2138

---
+++ src/main/java/org/fcrepo/importexport/ArgParser.java
@@ -0,0 +1,98 @@
+/*
+ * Licensed to DuraSpace under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * DuraSpace licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fcrepo.importexport;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
+/**
+ * @author awoods
+ * @since 2016-08-29
+ */
+public class ArgParser {
+
+    final private Options cmdOptions;
+
+    /**
+     * Constructor that creates the command line options
+     */
+    public ArgParser() {
+        // Command Line Options
+        cmdOptions = new Options();
+
+        // Help option
+        final Option helpOption = new Option("h", "help", false, "Print this message");
+        helpOption.setRequired(false);
+        cmdOptions.addOption(helpOption);
+
+        // Mode option
+        final Option importExportOption = new Option("m", "mode", true, "Mode: [import|export]");
+        importExportOption.setRequired(true);
+        importExportOption.setArgs(1);
+        importExportOption.setArgName("mode");
+        cmdOptions.addOption(importExportOption);
+    }
+
+    /**
+     * This method parses the command line options, returning the Import/Export configuration
+     *
+     * @param args from the command line
+     * @return Import/Export configuration
+     */
+    public Config parse(final String[] args) {
+        final Config config = new Config();
+
+        final CommandLineParser cmdParser = new DefaultParser();
+        try {
+            final CommandLine cmd = cmdParser.parse(cmdOptions, args);
+
+            // Inspect help option
+            if (cmd.hasOption('h')) {
+                printHelp("User Help");
+            }
+
+            // Inspect Mode option
+            final String mode = cmd.getOptionValue('m');
+            if (!mode.equalsIgnoreCase("import") && !mode.equalsIgnoreCase("export")) {
+                printHelp("Invalid 'mode' option: " + mode);
+            }
+
+            config.setMode(mode);
+
+        } catch (ParseException e) {
+            printHelp("Error parsing args: " + e.getMessage());
+        }
+
+        return config;
+    }
+
+    private void printHelp(final String message) {
+        System.out.println("\n-----------------------\n" + message + "\n-----------------------\n");
+
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp("Running Import/Export Utility", cmdOptions);
+
+        throw new RuntimeException();
+    }
+
+}
---
+++ src/main/java/org/fcrepo/importexport/Config.java
@@ -0,0 +1,54 @@
+/*
+ * Licensed to DuraSpace under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * DuraSpace licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.fcrepo.importexport;
+
+/**
+ * @author awoods
+ * @since 2016-08-29
+ */
+public class Config {
+
+    private String mode;
+
+    /**
+     * This method returns true if the configuration is set for 'import'
+     *
+     * @return true if import config
+     */
+    public boolean isImport() {
+        return mode.equalsIgnoreCase("import");
+    }
+
+    /**
+     * This method returns true if the configuration is set for 'export'
+     *
+     * @return true if export config
+     */
+    public boolean isExport() {
+        return !isImport();
+    }
+
+    /**
+     * This method sets the import/export mode
+     *
+     * @param mode of this configuration
+     */
+    public void setMode(final String mode) {
+        this.mode = mode;
+    }
+}
