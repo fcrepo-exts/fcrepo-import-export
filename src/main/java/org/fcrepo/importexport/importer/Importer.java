9708d3a22fe996222031b6fb1f242edcf01b620a
Esmé Cowles <escowles@ticklefish.org>
2017-05-17 09:37:35 -0400

    Fixing build after rebase

--- src/main/java/org/fcrepo/importexport/importer/Importer.java
+++ src/main/java/org/fcrepo/importexport/importer/Importer.java
@@ -65,6 +65,7 @@ import org.apache.commons.io.IOUtils;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.ResIterator;
+import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.rdf.model.StmtIterator;
 import org.apache.jena.riot.RDFDataMgr;
