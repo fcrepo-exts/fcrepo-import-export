de10479f68cbd4a5ef24b59a4ef10436ee250373
Esmé Cowles <escowles@ticklefish.org>
2017-05-30 16:36:23 -0400

    Filtering all inbound references, regardless of predicate (#102)
    * Filtering all inbound references, regardless of predicate
    
    Resolves: https://jira.duraspace.org/browse/FCREPO-2481 and https://jira.duraspace.org/browse/FCREPO-2500

---
+++ src/test/java/org/fcrepo/importexport/common/UriUtilsTest.java
@@ -0,0 +1,46 @@
+/*
+ * Licensed to DuraSpace under one or more contributor license agreements.
+ * See the NOTICE file distributed with this work for additional information
+ * regarding copyright ownership.
+ *
+ * DuraSpace licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except in
+ * compliance with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.fcrepo.importexport.common;
+
+import static org.junit.Assert.assertEquals;
+import java.net.URI;
+
+import org.junit.Test;
+
+/**
+ * @author escowles
+ * @since 2017-05-26
+ */
+public class UriUtilsTest {
+
+    private final URI withSlash = URI.create("http://localhost:8080/rest/");
+    private final URI withOutSlash = URI.create("http://localhost:8080/rest");
+
+    @Test
+    public void testWithSlash() throws Exception {
+        assertEquals(withSlash, UriUtils.withSlash(withSlash));
+        assertEquals(withSlash, UriUtils.withSlash(withOutSlash));
+    }
+
+    @Test
+    public void testWithoutSlash() throws Exception {
+        assertEquals(withOutSlash, UriUtils.withoutSlash(withSlash));
+        assertEquals(withOutSlash, UriUtils.withoutSlash(withOutSlash));
+    }
+}
