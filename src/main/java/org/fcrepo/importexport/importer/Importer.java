b286b1ae8b36c7b861b6992e8e1ec27d6eaa3bf5
Daniel Bernstein <dabylon@gmail.com>
2019-05-31 13:00:32 -0700

    Ensures that objects of rdf:type predicates in the ldp: namespace are removed before import. This fix also brings the ImporterIT integration tests back into the mix (ie they are no longer ignored).
    Resolves: https://jira.duraspace.org/browse/FCREPO-3001

--- src/main/java/org/fcrepo/importexport/importer/Importer.java
+++ src/main/java/org/fcrepo/importexport/importer/Importer.java
@@ -25,7 +25,6 @@ import static org.apache.jena.rdf.model.ResourceFactory.createResource;
 import static org.apache.jena.riot.RDFLanguages.contentTypeToLang;
 import static org.fcrepo.importexport.common.FcrepoConstants.BINARY_EXTENSION;
 import static org.fcrepo.importexport.common.FcrepoConstants.CONTAINS;
-import static org.fcrepo.importexport.common.FcrepoConstants.CONTAINER;
 import static org.fcrepo.importexport.common.FcrepoConstants.CREATED_BY;
 import static org.fcrepo.importexport.common.FcrepoConstants.CREATED_DATE;
 import static org.fcrepo.importexport.common.FcrepoConstants.DESCRIBEDBY;
@@ -35,10 +34,10 @@ import static org.fcrepo.importexport.common.FcrepoConstants.HAS_MIME_TYPE;
 import static org.fcrepo.importexport.common.FcrepoConstants.HAS_SIZE;
 import static org.fcrepo.importexport.common.FcrepoConstants.LAST_MODIFIED_BY;
 import static org.fcrepo.importexport.common.FcrepoConstants.LAST_MODIFIED_DATE;
+import static org.fcrepo.importexport.common.FcrepoConstants.LDP_NAMESPACE;
 import static org.fcrepo.importexport.common.FcrepoConstants.MEMBERSHIP_RESOURCE;
 import static org.fcrepo.importexport.common.FcrepoConstants.NON_RDF_SOURCE;
 import static org.fcrepo.importexport.common.FcrepoConstants.PAIRTREE;
-import static org.fcrepo.importexport.common.FcrepoConstants.RDF_SOURCE;
 import static org.fcrepo.importexport.common.FcrepoConstants.RDF_TYPE;
 import static org.fcrepo.importexport.common.FcrepoConstants.REPOSITORY_NAMESPACE;
 import static org.fcrepo.importexport.common.TransferProcess.fileForBinary;
@@ -574,10 +573,8 @@ public class Importer implements TransferProcess {
      * @return true if the resource represents a type that may not be added/removed explicitly
      */
     private boolean forbiddenType(final Resource resource) {
-         return resource.getNameSpace().equals(REPOSITORY_NAMESPACE)
-             || resource.getURI().equals(CONTAINER.getURI())
-             || resource.getURI().equals(NON_RDF_SOURCE.getURI())
-             || resource.getURI().equals(RDF_SOURCE.getURI());
+        return resource.getNameSpace().equals(REPOSITORY_NAMESPACE)
+            || resource.getNameSpace().equals(LDP_NAMESPACE);
     }
 
     /**
@@ -610,8 +607,7 @@ public class Importer implements TransferProcess {
         if (fileForBinaryURI(uri, false).exists() || fileForBinaryURI(uri, true).exists()) {
             response = client().put(uri).body(new ByteArrayInputStream(new byte[]{})).perform();
         } else if (fileForContainerURI(uri).exists()) {
-            response = client().put(uri).body(new ByteArrayInputStream(
-                    "<> a <http://www.w3.org/ns/ldp#Container> .".getBytes()), "text/turtle").perform();
+            response = client().put(uri).body(new ByteArrayInputStream("".getBytes()), "text/turtle").perform();
         } else {
             return;
         }
