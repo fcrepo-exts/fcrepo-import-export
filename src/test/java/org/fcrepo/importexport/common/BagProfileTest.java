36ae5c00802858339afaa443cee54a598100705d
Michael Ritter <shake@umiacs.umd.edu>
2020-02-11 16:53:12 -0700

    Add todo

--- src/test/java/org/fcrepo/importexport/common/BagProfileTest.java
+++ src/test/java/org/fcrepo/importexport/common/BagProfileTest.java
@@ -147,6 +147,7 @@ public class BagProfileTest {
     @Test
     @Ignore
     public void testAllProfilesPassValidation() throws IOException {
+        // TODO: the provided bag profiles need to be updated in order to pass validation
         final Path profiles = Paths.get("src/main/resources/profiles");
 
         Files.list(profiles).forEach(path -> {
