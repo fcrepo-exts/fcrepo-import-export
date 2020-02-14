e7077d1e610d8f19ead2b5e07ce962d8a08e45c2
Michael Ritter <shake@umiacs.umd.edu>
2020-02-13 17:13:45 -0700

    Add common type mappings for various mime types
    For example, application/gtar would be seen as application/tar,
    since they are functionally the same when doing the extraction.

--- src/main/java/org/fcrepo/importexport/common/SerializationSupport.java
+++ src/main/java/org/fcrepo/importexport/common/SerializationSupport.java
@@ -26,7 +26,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -39,19 +41,60 @@ public class SerializationSupport {
 
     private static final Logger logger = LoggerFactory.getLogger(SerializationSupport.class);
 
-    public static final Set<String> ZIP_TYPES = Collections.singleton("application/zip");
-    public static final Set<String> TAR_TYPES = new HashSet<>(Arrays.asList("application/tar", "application/x-tar"));
-    public static final Set<String> GZIP_TYPES = new HashSet<>(Arrays.asList("application/gzip", "application/x-gzip"));
-    public static final Set<String> SEVEN_ZIP_TYPES = Collections.singleton("application/x-7zip-compressed");
+    // zip
+    private static final String APPLICATION_ZIP = "application/zip";
+
+    // tar + gtar
+    private static final String APPLICATION_TAR = "application/tar";
+    private static final String APPLICATION_GTAR = "application/gtar";
+    private static final String APPLICATION_X_TAR = "application/x-tar";
+    private static final String APPLICATION_X_GTAR = "application/x-gtar";
+
+    // gzip
+    private static final String APPLICATION_GZIP = "application/gzip";
+    private static final String APPLICATION_X_GZIP = "application/x-gzip";
+    private static final String APPLICATION_X_COMPRESSED_TAR = "application/x-compressed-tar";
+
+    // 7zip
+    // private static final String APPLICATION_X_7Z_COMPRESSED = "application/x-7zip-compressed";
+
+    public static final Set<String> ZIP_TYPES = Collections.singleton(APPLICATION_ZIP);
+    public static final Set<String> TAR_TYPES = new HashSet<>(Arrays.asList(APPLICATION_TAR, APPLICATION_X_TAR,
+                                                                            APPLICATION_GTAR, APPLICATION_X_GTAR));
+    public static final Set<String> GZIP_TYPES = new HashSet<>(Arrays.asList(APPLICATION_GZIP, APPLICATION_X_GTAR,
+                                                                             APPLICATION_X_COMPRESSED_TAR));
+    // public static final Set<String> SEVEN_ZIP_TYPES = Collections.singleton("application/x-7zip-compressed");
+
+    private static Map<String, String> commonTypeMap = initCommonTypeMapping();
 
     private SerializationSupport() {
     }
 
     /**
+     * Just a way to instantiate the {@code commonTypeMap}
+     *
+     * @return the map of supported application types
+     */
+    private static Map<String, String> initCommonTypeMapping() {
+        commonTypeMap = new HashMap<>();
+        commonTypeMap.put(APPLICATION_ZIP, APPLICATION_ZIP);
+
+        commonTypeMap.put(APPLICATION_TAR, APPLICATION_TAR);
+        commonTypeMap.put(APPLICATION_GTAR, APPLICATION_TAR);
+        commonTypeMap.put(APPLICATION_X_TAR, APPLICATION_X_TAR);
+        commonTypeMap.put(APPLICATION_X_GTAR, APPLICATION_X_TAR);
+
+        commonTypeMap.put(APPLICATION_GZIP, APPLICATION_GZIP);
+        commonTypeMap.put(APPLICATION_X_GZIP, APPLICATION_GZIP);
+        commonTypeMap.put(APPLICATION_X_COMPRESSED_TAR, APPLICATION_GZIP);
+        return commonTypeMap;
+    }
+
+    /**
      * Get a {@link BagDeserializer} for a given content type. Currently supported are:
-     * zip - {@link ZipBagDeserializer}
-     * tar - {@link TarBagDeserializer}
-     * tar+gz - {@link GZipBagDeserializer}
+     * zip - {@link SerializationSupport#ZIP_TYPES} -> {@link ZipBagDeserializer}
+     * tar - {@link SerializationSupport#TAR_TYPES} -> {@link TarBagDeserializer}
+     * tar+gz - {@link SerializationSupport#GZIP_TYPES} -> {@link GZipBagDeserializer}
      *
      * @param serializedBag the Bag (still serialized) to get a {@link BagDeserializer} for
      * @param profile the {@link BagProfile} to ensure that the content type is allowed
@@ -64,7 +107,8 @@ public class SerializationSupport {
         final String contentType;
 
         try {
-            contentType = tika.detect(serializedBag);
+            final String detectedType = tika.detect(serializedBag);
+            contentType = commonTypeMap.getOrDefault(detectedType, detectedType);
             logger.debug("{}: {}", serializedBag, contentType);
         } catch (IOException e) {
             logger.error("Unable to get content type for {}", serializedBag);
