5eda1f87a6953a5e8293b1435e65638451880b14
Michael Ritter <shake@umiacs.umd.edu>
2020-02-26 11:00:05 -0700

    Change to a parameterized test

--- src/test/java/org/fcrepo/importexport/common/BagDeserializerTest.java
+++ src/test/java/org/fcrepo/importexport/common/BagDeserializerTest.java
@@ -28,11 +28,17 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Objects;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 /**
  * Test that zip, tar, and tar+gz extraction works as expected
@@ -40,14 +46,30 @@ import org.junit.Test;
  * @author mikejritter
  * @since 2020-02-13
  */
+@RunWith(Parameterized.class)
 public class BagDeserializerTest {
+
+    @Parameters(name = "extract {0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+            {"bag-tar.tar", "bag-tar"}, {"bag-zip.zip", "bag-zip"}, {"bag-gzip.tar.gz", "bag-gzip"},
+            {"bag-tar-mismatch.tar", "bag-tar"}
+        });
+    }
 
     public static final String BEYONDTHEREPOSITORY_JSON = "src/main/resources/profiles/beyondtherepository.json";
     public static final String BAG_INFO_TXT = "bag-info.txt";
     public static final String DATA_DIR = "data";
-    private final String expectedDir = "bag";
     private final String group = "compress";
     private Path target;
+
+    private final String archive;
+    private final String expectedDir;
+
+    public BagDeserializerTest(final String archive, final String expectedDir) {
+        this.archive = archive;
+        this.expectedDir = expectedDir;
+    }
 
     @Before
     public void setup() throws URISyntaxException {
@@ -56,14 +78,14 @@ public class BagDeserializerTest {
         assertNotNull(target);
     }
 
-    private void cleanup(final String test) throws IOException {
-        FileUtils.deleteDirectory(target.resolve(group).resolve(expectedDir + "-" + test).toFile());
+    @After
+    public void cleanup() throws IOException {
+        FileUtils.deleteDirectory(target.resolve(group).resolve(expectedDir).toFile());
     }
 
     @Test
-    public void testExtractZip() throws IOException {
-        final String serializedBag = expectedDir + "-zip.zip";
-        final Path path = target.resolve(group).resolve(serializedBag);
+    public void testExtract() {
+        final Path path = target.resolve(group).resolve(archive);
         try {
             final BagProfile profile = new BagProfile(Files.newInputStream(Paths.get(BEYONDTHEREPOSITORY_JSON)));
             final BagDeserializer deserializer = SerializationSupport.deserializerFor(path, profile);
@@ -72,69 +94,11 @@ public class BagDeserializerTest {
             fail("Unexpected exception:\n" + e.getMessage());
         }
 
-        final Path bag = target.resolve(group).resolve(expectedDir + "-zip");
+        final Path bag = target.resolve(group).resolve(expectedDir);
         assertTrue(Files.exists(bag));
         assertTrue(Files.exists(bag.resolve(BAG_INFO_TXT)));
         assertTrue(Files.exists(bag.resolve(DATA_DIR)));
         assertTrue(Files.isDirectory(bag.resolve(DATA_DIR)));
-        cleanup("zip");
     }
 
-    @Test
-    public void testExtractTar() throws IOException {
-        final String serializedBag = expectedDir + "-tar.tar";
-        final Path path = target.resolve(group).resolve(serializedBag);
-        try {
-            final BagProfile profile = new BagProfile(Files.newInputStream(Paths.get(BEYONDTHEREPOSITORY_JSON)));
-            final BagDeserializer deserializer = SerializationSupport.deserializerFor(path, profile);
-            deserializer.deserialize(path);
-        } catch (IOException e) {
-            fail("Unexpected exception:\n" + e.getMessage());
-        }
-
-        final Path bag = target.resolve(group).resolve(expectedDir + "-tar");
-        assertTrue(Files.exists(bag));
-        assertTrue(Files.exists(bag.resolve(BAG_INFO_TXT)));
-        assertTrue(Files.exists(bag.resolve(DATA_DIR)));
-        assertTrue(Files.isDirectory(bag.resolve(DATA_DIR)));
-        cleanup("tar");
-    }
-
-    @Test
-    public void testExtractTarMismatchName() throws IOException {
-        final String serializedBag = expectedDir + "-tar-mismatch.tar";
-        final Path path = target.resolve(group).resolve(serializedBag);
-        try {
-            final BagProfile profile = new BagProfile(Files.newInputStream(Paths.get(BEYONDTHEREPOSITORY_JSON)));
-            final BagDeserializer deserializer = SerializationSupport.deserializerFor(path, profile);
-            deserializer.deserialize(path);
-        } catch (IOException e) {
-            fail("Unexpected exception:\n" + e.getMessage());
-        }
-
-        final Path bag = target.resolve(group).resolve(expectedDir + "-tar");
-        assertTrue(Files.exists(bag));
-        assertTrue(Files.exists(bag.resolve(BAG_INFO_TXT)));
-        cleanup("tar");
-    }
-
-    @Test
-    public void testExtractGZip() throws IOException {
-        final String serializedBag = expectedDir + "-gzip.tar.gz";
-        final Path path = target.resolve(group).resolve(serializedBag);
-        try {
-            final BagProfile profile = new BagProfile(Files.newInputStream(Paths.get(BEYONDTHEREPOSITORY_JSON)));
-            final BagDeserializer deserializer = SerializationSupport.deserializerFor(path, profile);
-            deserializer.deserialize(path);
-        } catch (IOException e) {
-            fail("Unexpected exception:\n" + e.getMessage());
-        }
-
-        final Path bag = target.resolve(group).resolve(expectedDir + "-gzip");
-        assertTrue(Files.exists(bag));
-        assertTrue(Files.exists(bag.resolve(BAG_INFO_TXT)));
-        assertTrue(Files.exists(bag.resolve(DATA_DIR)));
-        assertTrue(Files.isDirectory(bag.resolve(DATA_DIR)));
-        cleanup("gzip");
-    }
 }
