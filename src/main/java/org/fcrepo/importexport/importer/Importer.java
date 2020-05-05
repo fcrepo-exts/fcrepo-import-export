84f4eeeccd8365c8873d1da602ca7776b86d1e0e
Michael Ritter <emericaskate15@gmail.com>
2020-05-05 17:01:16 -0600

    Remove unused import

--- src/main/java/org/fcrepo/importexport/importer/Importer.java
+++ src/main/java/org/fcrepo/importexport/importer/Importer.java
@@ -65,7 +65,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
