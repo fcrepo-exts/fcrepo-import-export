4033cd4972e2fda8cab97c58aef537792d0b0161
Michael Ritter <emericaskate15@gmail.com>
2020-05-29 14:40:55 -0600

    Remove unused import

--- src/test/java/org/fcrepo/importexport/integration/BagItIT.java
+++ src/test/java/org/fcrepo/importexport/integration/BagItIT.java
@@ -41,7 +41,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.MessageDigest;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
